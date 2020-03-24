0607919fde1184ae80485d458a88d43605b310be
Milos Bobic <milos.bobic@quantox.com>
2020-03-24 16:54:36 +0100

    added variable and output

--- main.tf
+++ main.tf
@@ -9,8 +9,9 @@ resource "aws_instance" "my_example" {
   user_data = <<-EOF
             #!/bin/bash
             echo "Hello, World" > index.html
-            nohup busybox httpd -f -p 8080 &
+            nohup busybox httpd -f -p ${var.server_port} &
             EOF
+  # this tag adds the name of an instance
   tags = {
       Name = "terraform-my-example"
   }
@@ -20,14 +21,24 @@ resource "aws_instance" "my_example" {
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
     ingress {
-        from_port = 8080
-        to_port = 8080
+        from_port = var.server_port
+        to_port = var.server_port
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
 
+output "public_ip" {
+    value = aws_instance.my_example.public_ip
+    description = "The public IP address of the web server"
+}
 #expressions
 # - reference: <PROVIDER>_<TYPE>.<NAME>.<ATTRIBUTE> they are used to refference other resources to existing ones, usually by id
 # command - terraform graph - gives output that can be drawn here GraphvizOnline - http://bit.ly/2mPbxmg
+# variable types: string, number, bool, list, map, set, object, tuple and any. Default "any"
